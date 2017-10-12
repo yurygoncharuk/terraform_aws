a9e32e415d890a6f72da73820113e4ae4555291b
Yury Hancharuk <root@ip-172-31-29-180.ec2.internal>
2017-10-12 10:14:28 +0000

    update s3 module

--- s3/main.tf
+++ s3/main.tf
@@ -2,18 +2,3 @@ resource "aws_s3_bucket" "log_bucket" {
   bucket = "elb-tf-log-bucket"
   acl    = "log-delivery-write"
 }
-
-output "bucket_id" {
-  value = "${aws_s3_bucket.log_bucket.id}"
-}
-/*
-resource "aws_s3_bucket" "b" {
-  bucket = "elb-tf-test-bucket"
-  acl    = "private"
-
-  logging {
-    target_bucket = "${aws_s3_bucket.log_bucket.id}"
-    target_prefix = "log/"
-  }
-}
-*/
